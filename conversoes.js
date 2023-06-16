acafb2d4c3a902dc35b8350a26040e6583c9c777
Gustavo Muniz <gustavosbu321@gmail.com>
2023-06-16 16:14:12 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversao implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+//number()
+//string()
+console.log(numero + numeroString);
+
+// conversao explícita
